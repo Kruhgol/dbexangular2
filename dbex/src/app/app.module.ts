56b5952c299b3881a792a1f8fe60e991dd857d8c
kruhol <n.kruhol@gmail.com>
2018-08-14 10:03:14 +0300

    PEX-3-add-states. fix libriary errors

--- dbex/src/app/app.module.ts
+++ dbex/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UIRouterModule } from '@uirouter/angular';
+import { APP_STATES } from './app.states';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UIRouterModule.forRoot({
+      states: APP_STATES,
+      useHash: true,
+      // otherwise: {
+      //   state: 'home'
+      // }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ dbex/src/app/app.states.ts
@@ -0,0 +1,17 @@
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login/login.component";
+
+export const appState = {
+    name: 'app',
+    component: AppComponent
+};
+
+export const loginState = {
+    name: 'login',
+    url: 'login',
+    component: LoginComponent
+}
+
+export const APP_STATES = [
+    appState
+];
