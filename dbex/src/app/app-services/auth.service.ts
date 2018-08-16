b9dd7ac0a01e68d066b35d11ca9d9dc56c2fdea9
kruhol <n.kruhol@gmail.com>
2018-08-16 17:33:40 +0300

    PEX-5-auth-service. add localStorage methods

--- dbex/src/app/app-services/auth.service.ts
+++ dbex/src/app/app-services/auth.service.ts
@@ -17,16 +17,9 @@ export class AuthService {
 
     return this.http.post('http://test.dbex.org/api/token', data.join('&') , {
       headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
-    }).subscribe(
-      (data: any) => {
-        return this.http.post('http://test.dbex.org/api/category/documentsbyfilter', {}, {
-          headers: new HttpHeaders().set('Authorization', 'Bearer ' + data.access_token)
-        }).subscribe((data: any) => {
-          console.log(data);
-        })
-      }
-    );
+    }).subscribe(data => {
+      console.log(data);
+        window.localStorage.setItem('appStorageData', JSON.stringify(data));
+    });
   }
-
-
 }
